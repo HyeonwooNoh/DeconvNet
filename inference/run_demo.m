88ee00fe877b7a65629d122eee25aae6a1040bb7
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2015-06-18 14:24:27 +0900

    inference script is modified

--- inference/run_demo.m
+++ inference/run_demo.m
@@ -18,13 +18,14 @@ config.im_sz = 500;
 cache_FCN8s_results(config);
 
 %% generate EDeconvNet+CRF results
-%% user configuration
-config.write_file = 1;
 
-config.edgebox_cache_dir = './data/edge_box_cached/VOC2012_TEST';
+config.write_file = 1;
+config.edgebox_cache_dir = './data/edgebox_cached/VOC2012_TEST';
 config.Path.CNN.script_path = './DeconvNet';
 config.Path.CNN.model_data = [config.Path.CNN.script_path '/DeconvNet_trainval_inference.caffemodel'];
 config.Path.CNN.model_proto = [config.Path.CNN.script_path '/DeconvNet_inference_deploy.prototxt'];
 config.max_proposal_num = 50;
 config.im_sz = 224;
 config.fcn_score_dir = './results/FCN8s';
+
+generate_EDeconvNet_CRF_results(config);
